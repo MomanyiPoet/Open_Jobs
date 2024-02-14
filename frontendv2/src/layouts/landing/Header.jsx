7cc8aca3fd0aba20481b2abd6124cdd076e18ebe
Momanyi Poet <momanyiturner@gmail.com>
2024-02-14 17:11:02 +0300

    added seo alt

--- frontendv2/src/layouts/landing/Header.jsx
+++ frontendv2/src/layouts/landing/Header.jsx
@@ -40,8 +40,8 @@ function Header() {
             {/* Left */}
             <div className="flex flex-col space-y-4">
                 <div className="flex flex-row justify-evenly xl:justify-between">
-                <img src={openJobs3} alt="" className="w-[200px] sm:w-[280px] md:w-[350px] lg:w-[240px] xl:w-[300px] rounded-3xl shadow-2xl"/>
-                    <img src={openJobs2} alt="" className="w-[200px] sm:w-[280px] md:w-[350px] lg:w-[240px] xl:w-[300px] rounded-3xl shadow-2xl"/>
+                    <img src={openJobs3} alt="Opportunities Meet Aspirations" title='Kenya Jobs | Open Jobs' className="w-[200px] sm:w-[280px] md:w-[350px] lg:w-[240px] xl:w-[300px] rounded-3xl shadow-2xl"/>
+                    <img src={openJobs2} alt="Opportunities Meet Aspirations" title='Kenya Jobs | Open Jobs' className="w-[200px] sm:w-[280px] md:w-[350px] lg:w-[240px] xl:w-[300px] rounded-3xl shadow-2xl"/>
                 </div>
                 <div className="bg-gradient-to-r from-cyan-600 to-blue-500 rounded-3xl py-6 px-6 sm:px-8 shadow-2xl">
                     <p className="text-whity text-sm md:text-base">
@@ -77,7 +77,7 @@ function Header() {
                             {displayedArticles.map((article) => (
                                 <div className="py-1 hover:underline" key={article.id}>
                                     <a href="#" className="flex items-center justify-start space-x-4">
-                                        <div className="p-12 sm:p-10 rounded-full bg-center bg-cover shadow-2xl transition duration-200 transform hover:scale-110" style={{backgroundImage: `url(${article.image})`,}}>
+                                        <div className="p-12 sm:p-10 rounded-full bg-center bg-cover shadow-2xl transition duration-200 transform hover:scale-110" style={{backgroundImage: `url(${article.image})`,}} alt="Opportunities Meet Aspirations" title='Kenya Jobs | Open Jobs'>
                                         </div>
                                         <div className="flex flex-col justify-between">
                                             <h6 className="text-secondary font-semibold text-lg">{article.title}</h6>
